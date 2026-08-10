7ed305814421a3c1f4a16e3bb11454204978457b
otacon <orfeo18@gmail.com>
2026-08-10 07:02:42 +0100

    Bump version

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -30,7 +30,7 @@ val propertyVersion = providers.gradleProperty("appVersion")
 val isRelease = propertyVersion.orNull != null
 
 val appVersion = propertyVersion
-    .orElse("0.1.1-indev")
+    .orElse("0.1.2-indev")
     .map { it.removePrefix("v") }
     .get()
 
