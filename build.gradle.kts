f58a03594a179e2365fbfa5c00d8634378460df3
Orfeo Ciano <orfeo18@gmail.com>
2026-08-26 09:32:16 +0100

    Add tests (#14)
    * Add ops tests
    
    * Add PR Review

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.buildKonfig) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.composeMultiplatform) apply false
+    alias(libs.plugins.kotest) apply false
+    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlinxSerialization) apply false
